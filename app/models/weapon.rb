d78ec8d520ea93809ca8ff24bac495ee7dd4cd52
diegocorrea <diegocorrea.cc@gmail.com>
2016-09-22 20:34:33 -0300

    add validates model in weapons

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,2 +1,4 @@
 class Weapon < ActiveRecord::Base
+	validates :model, :factory, presence: true, length: { maximum: 20 }
+	validates :serialNumber, presence: true, numericality: { only_integer: true }, uniqueness: { case_sensitive: false }, :numericality => { :greater_than_or_equal_to => 1 }
 end
