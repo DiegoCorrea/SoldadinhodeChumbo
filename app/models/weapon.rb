66cb8ce8605549ad72221958251157417c349afc
diegocorrea <diegocorrea.cc@gmail.com>
2016-09-23 03:23:35 -0300

    finish validates model munitions, weapons and accessories

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,6 +1,7 @@
 class Weapon < ActiveRecord::Base
-	validates :model, :factory, presence: true, length: { maximum: 20 }
+	validates :model, :factory, presence: true, length: { maximum: 127 }
 	validates :serialNumber, presence: true, numericality: { only_integer: true }, uniqueness: { case_sensitive: false }, :numericality => { :greater_than_or_equal_to => 1 }
+	validates :allocated, inclusion: { in: [ true, false ] }
 
 	has_many :reserve_material
 end
