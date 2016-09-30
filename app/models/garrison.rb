5b319aa21a1adebd5693f4e274e53574c53bbae5
diegocorrea <diegocorrea.cc@gmail.com>
2016-09-30 13:47:57 -0300

    link db reserve garrison weapons

--- app/models/garrison.rb
+++ app/models/garrison.rb
@@ -1,5 +1,6 @@
 class Garrison < ActiveRecord::Base
   belongs_to :reserve
+  
   has_many :weapons
   has_many :munitions
   has_many :accessories
--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -1,4 +1,8 @@
 class Reserve < ActiveRecord::Base
 	has_many :soldiers
 	has_one :garrison
+
+	has_many :weapons, through: :garrison
+	has_many :munitions, through: :garrison
+	has_many :accessories, through: :garrison
 end
