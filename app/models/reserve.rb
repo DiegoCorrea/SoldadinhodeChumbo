d4f2f79e0d54dc1e4e6a22367d8e242a5d398d6d
diegocorrea <diegocorrea.cc@gmail.com>
2016-10-01 01:26:30 -0300

    add depency to destroy reserve

--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -1,10 +1,10 @@
 class Reserve < ActiveRecord::Base
-	has_many :soldiers
+	has_many :soldiers, :dependent => :destroy
 
 	
-	has_one :garrison
+	has_one :garrison, :dependent => :destroy
 
-	has_many :weapons, through: :garrison
-	has_many :munitions, through: :garrison
-	has_many :accessories, through: :garrison
+	has_many :weapons, through: :garrison, :dependent => :destroy
+	has_many :munitions, through: :garrison, :dependent => :destroy
+	has_many :accessories, through: :garrison, :dependent => :destroy
 end
