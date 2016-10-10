a99487fb5ce63737e008ae1446ea718e2cb201eb
diegocorrea <diegocorrea.cc@gmail.com>
2016-10-10 18:16:03 -0300

    text adapter

--- app/models/munition.rb
+++ app/models/munition.rb
@@ -1,5 +1,5 @@
 class Munition < ActiveRecord::Base
-	validates :caliber, presence: true, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }, uniqueness: { case_sensitive: false }
+	validates :caliber, presence: true, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
 	validates :description, presence: true
 	validates :amount, presence: true, numericality: { only_integer: true, :greater_than_or_equal_to => 0}
 	
